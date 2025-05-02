e7a23f16ddab292fe344ac9e7789ea9ee8f0c462
Brian Mosbeux <b.mosbeux@gmail.com>
2025-05-02 14:39:50 +0200

    In the save_markdown_file function, ensure structures data is a valid JSON

--- migrate.py
+++ migrate.py
@@ -141,6 +141,7 @@ def save_to_json(posts, filename="output/wordpress_posts.json"):
 
 def save_markdown_file(post):
     filename = f"output/posts/{post['slug']}.md"
+    structured_data_str = json.dumps(post["structured_data"], ensure_ascii=False, indent=2) if post["structured_data"] else "null"
     os.makedirs(os.path.dirname(filename), exist_ok=True)
     
     # Prepare the markdown file content
@@ -170,7 +171,7 @@ seo:
     description: {post['twitter_description']}
     image: {post['twitter_image']}
     creator: {post['twitter_creator']}
-  structured_data: {post['structured_data']}  
+  structured_data: {structured_data_str}  
 ---
 
 # {post['title']}
